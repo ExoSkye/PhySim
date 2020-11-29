b41e7574086e91d9c03867ac5dac2b0fa8151058
protobit <admin@protobyte.tech>
2020-11-29 18:01:53 +0000

    Added one dot on windows code

--- tests/test_linking/test.cpp
+++ tests/test_linking/test.cpp
@@ -6,7 +6,7 @@ int main() {
 #ifdef __linux__
     void* dlhandle = physim::loadLib("../examples/law/libGravityLaw.so");
 #elif _WINDOWS
-    HINSTANCE dlhandle = physim::loadLib("./examples/law/GravityLaw.dll");
+    HINSTANCE dlhandle = physim::loadLib("../examples/law/GravityLaw.dll");
 #endif
     if (dlhandle != NULL) {
         printf("All ok");
