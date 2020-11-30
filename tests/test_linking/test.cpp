f8cb5ad62ae37f234b7be504207cec1e34fadc41
ProtoBit <admin@protobyte.tech>
2020-11-30 10:19:09 +0000

    Hopefully fixed testing on windows

--- tests/test_linking/test.cpp
+++ tests/test_linking/test.cpp
@@ -4,9 +4,9 @@
 #include <dl/load.hpp>
 int main() {
 #ifdef __linux__
-    void* dlhandle = physim::loadLib("../examples/law/libGravityLaw.so");
+    void* dlhandle = physim::loadLib(CMAKE_BINARY_DIR"/../examples/law/libGravityLaw.so");
 #elif _WINDOWS
-    HINSTANCE dlhandle = physim::loadLib("../examples/law/GravityLaw.dll");
+    HINSTANCE dlhandle = physim::loadLib(CMAKE_BINARY_DIR"/../examples/law/GravityLaw.dll");
 #endif
     if (dlhandle != NULL) {
         printf("All ok");
